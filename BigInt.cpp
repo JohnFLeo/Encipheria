c7aad88fcf0f89f49f10d5d9645be5dc6917688a
JohnFLeo <jonathan.leopold03@gmail.com>
2026-04-13 20:25:58 +0200

    docs: add docstrings for add operation #2

--- BigInt.cpp
+++ BigInt.cpp
@@ -65,7 +65,38 @@ void BigInt::setNumber(const std::string &str) {
     }
 }
 
-
+/* Case: a.size < b.size
+ * a = [ a0 ] [ a1 ]
+ * b = [ b0 ] [ b1 ] [ b2 ] [ b3 ] [ b4 ]
+ * idxA starts at size of a minus one and gets smaller the bigger i gets (analog for idxB)
+ * idxA and idxB are the same distance away from their ending
+ * example:
+ *   i = 1 -> a[idxA] = [ a0 ], b[idxB] = [ b3 ]
+ *   i = 3 -> a[idxA] =  null , b[idxB] = [ b1 ] etc.
+ *
+ * after i = 2: idxA < 0 and thus the number b[idxB] is inserted at the front of a
+ *
+ * Case: a.size >= b.size
+ * a = [ a0 ] [ a1 ] [ a2 ] [ a3 ] [ a4 ]
+ * b = [ b0 ] [ b1 ]
+ * before i = 2: idxB >  0 and thus an addition is performed
+ * example:
+ *   i = 1 -> a[idxA] = a[idxA] + b[idxB] =[ a3 ]+ [ b0 ]
+ * after  i = 2: idxB <= 0 and thus
+ * example:
+ *   i = 3 -> a[idxA] = a[idxA] + null] =[ a3 ]+ 0
+ *
+ * when an addition is performed the carry if present is collected and returned
+ */
+/// adds two entries and cleans invalid inputs
+/// - if idxA is invalid then a is extended in the front
+/// - if idxB is invalid then nothing needs to be done
+/// - if both indexes are valid a normal addition is performed
+/// @param a number that receives the result and acts as operand
+/// @param b other operand
+/// @param idxA index points to value that should be added (can be less than 0)
+/// @param idxB index points to value that should be added (can be less than 0)
+/// @return true if an addition resulted in an overflow
 bool addBIfPossible(BigInt &a, const BigInt &b, const int idxA, const int idxB) {
     if(idxA<0) {
         a.number.push_front(b.number.at(idxB));
@@ -76,6 +107,12 @@ bool addBIfPossible(BigInt &a, const BigInt &b, const int idxA, const int idxB) 
     }
     return __builtin_add_overflow(a.number.at(idxA),b.number.at(idxB), &a.number.at(idxA));
 }
+/// adds 1 to an entry and cleans invalid inputs
+/// - if idxA is invalid then a is extended in the front
+/// - if idxA is valid a normal addition is performed
+/// @param a number that receives the result and acts as operand
+/// @param idxA index points to value that should be added (can be less than 0)
+/// @return true if an addition resulted in an overflow
 bool addOneIfPossible(BigInt &a,  const int idxA) {
     if(idxA<0) {
         a.number.push_front(1);
@@ -84,6 +121,22 @@ bool addOneIfPossible(BigInt &a,  const int idxA) {
     return __builtin_add_overflow(a.number.at(idxA),1, &a.number.at(idxA));
 }
 
+/// The addition is performed by iterating over all indexes of the number with a higher length
+/// Two indexes are used to keep track of the numbers that should be added together
+/// - idxA starts at size of a minus one and gets smaller the bigger i gets (analog for idxB)
+/// - idxA and idxB are the same distance away from their numbers ending
+/// - example:
+///  @code
+///  a = [ a0 ] [ a1 ]
+///  b = [ b0 ] [ b1 ] [ b2 ] [ b3 ] [ b4 ]
+///   i = 1 -> a[idxA] = [ a0 ], b[idxB] = [ b3 ]
+///   i = 3 -> a[idxA] =  null , b[idxB] = [ b1 ] etc.
+///  @endcode
+/// - adding is performed by helper methods that catch the possible negative indexes
+/// - if a carry happened during one iteration an additional 1 is added to the number
+/// - at the end if a carry is still present it is written into a new number that gets inserted at the front
+/// @param a receives the result of the addition and acts as an operand
+/// @param b other operand
 void addAB(BigInt &a, const BigInt &b) {
     const size_t bigger = std::max( a.number.size(),b.number.size() );
     bool carry = false;
@@ -98,6 +151,9 @@ void addAB(BigInt &a, const BigInt &b) {
     if(carry) a.number.push_front(1);
 }
 
+/// Performs an addition that is constructed by adding the corresponding entries of two BigInts
+/// This is performed by individual adding and keeping track of overflows
+/// @param other the other operand
 void BigInt::operator+=(const BigInt &other) {
     std::cout << " ";
     displayHex();
